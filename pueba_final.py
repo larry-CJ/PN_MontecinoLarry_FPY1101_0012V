ae9c5ec4e0503c964d6f358fa8237dd62e68c632
larry-CJ <la.montecino@duocuc.cl>
2024-07-17 20:43:38 -0400

    comparación de saldos

--- pueba_final.py
+++ pueba_final.py
@@ -13,8 +13,7 @@ def clasificar_sueldos(sueldos):
     rango_bajo = []
     rango_medio = []
     rango_alto = []
-    
-    for i, sueldo in enumerate(sueldos):
+   for i, sueldo in enumerate(sueldos):
         if sueldo < 800000:
             rango_bajo.append((trabajadores[i], sueldo))
         elif 800000 <= sueldo <= 2000000:
@@ -22,7 +21,26 @@ def clasificar_sueldos(sueldos):
         else:
             rango_alto.append((trabajadores[i], sueldo))
     
-
+    print("Sueldos menores a $800.000")
+    print("TOTAL:", len(rango_bajo))
+    print("Nombre empleado\tSueldo")
+    for nombre, sueldo in rango_bajo:
+        print(f"{nombre}\t${sueldo}")
+    
+    print("\nSueldos entre $800.000 y $2.000.000")
+    print("TOTAL:", len(rango_medio))
+    print("Nombre empleado\tSueldo")
+    for nombre, sueldo in rango_medio:
+        print(f"{nombre}\t${sueldo}")
+    
+    print("\nSueldos superiores a $2.000.000")
+    print("TOTAL:", len(rango_alto))
+    print("Nombre empleado\tSueldo")
+    for nombre, sueldo in rango_alto:
+        print(f"{nombre}\t${sueldo}")
+    
+    print("TOTAL SUELDOS: $", sum(sueldos))
+    
 def mostrar_menu():
     print("________Menú:________")
     print("1. Asignar sueldos aleatorios")
